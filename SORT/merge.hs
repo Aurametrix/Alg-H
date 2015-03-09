d27e19b2e0953cab7beb7dca2712868ee7647444
Aurametrix <irene@aurametrix.com>
2015-03-08 19:06:31 -0700

    merging 2 sorted arrays

--- SORT/merge.hs
+++ SORT/merge.hs
@@ -1,3 +1,11 @@
 mergesort'splitinhalf :: [a] -> ([a], [a])
 mergesort'splitinhalf xs = (take n xs, drop n xs)
     where n = (length xs) `div` 2 
+    
+    
+mergesort'merge :: (Ord a) => [a] -> [a] -> [a]
+mergesort'merge [] xs = xs
+mergesort'merge xs [] = xs
+mergesort'merge (x:xs) (y:ys)
+    | (x < y) = x:mergesort'merge xs (y:ys)
+    | otherwise = y:mergesort'merge (x:xs) ys
